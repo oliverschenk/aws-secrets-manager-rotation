189467c49c9dd0de8148ea70f062453e111ed9b8
Oliver Schenk <oliver.schenk@gmail.com>
2022-05-18 21:00:49 +0800

    Script aws-vault optional

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ DESTROY_ACTION='destroy'
 DEFAULT_REGION='ap-southeast-2'
 DEFAULT_STAGE='dev'
 
+AWS_VAULT_PREFIX=''
+
 REGION=$DEFAULT_REGION
 STAGE=$DEFAULT_STAGE
 ACTION=$DEPLOY_ACTION
@@ -18,13 +20,17 @@ function usage {
     echo "  Script for deploying serverless lambda."
     echo ""
     echo "USAGE:"
-    echo "  deploy.sh -p credentials_profile [-r region] [-s stage] [-d destroy]"
+    echo "  deploy.sh [-p credentials_profile] [-r region] [-s stage] [-d destroy]"
     echo ""
     echo "OPTIONS"
     echo "  -p   the credentials profile to use (uses aws-vault)"
     echo "  -r   region (default: ap-southeast-2)"
     echo "  -s   the stage to deploy [dev, test, prod] (default: dev)"
     echo "  -d   destroy"
+}
+
+function aws_exec {
+    ${AWS_VAULT_PREFIX}$1
 }
 
 while getopts "p:r:s:d" option; do
@@ -41,30 +47,29 @@ while getopts "p:r:s:d" option; do
     esac
 done
 
-if [[ -z "${AWS_VAULT_PROFILE}" ]]; then
-    echo "Please provide the aws-vault profile as -p profile" 1>&2
-    VALIDATION_ERROR=1
-fi
 if [[ -n "${VALIDATION_ERROR}" ]]; then
     usage
     exit 1
 fi
 
+if [[ -n "${AWS_VAULT_PROFILE}" ]]; then
+    AWS_VAULT_PREFIX="aws-vault exec ${AWS_VAULT_PROFILE} -- "
+fi
+
 echo "=== Using the following parameters ==="
 echo "Region: ${REGION}"
 echo "Stage: ${STAGE}"
-echo "Profile: ${AWS_VAULT_PROFILE}"
 echo "Action: ${ACTION}"
 
 echo ""
 echo "=== Applying action: ${ACTION} ==="
-aws-vault exec ${AWS_VAULT_PROFILE} -- terraform ${ACTION} --var aws_region=${REGION}
+aws_exec "terraform ${ACTION} --var aws_region=${REGION}"
 
 if [[ "${ACTION}" = "${DESTROY_ACTION}" ]]; then
 
   echo ""
   echo "=== Forcing deletion of secret ==="
-  aws-vault exec ${AWS_VAULT_PROFILE} -- aws secretsmanager delete-secret --force-delete-without-recovery --secret-id /dev/my-project/database/secret
+  aws_exec "aws secretsmanager delete-secret --force-delete-without-recovery --secret-id /dev/my-project/database/secret"
 
 fi
 
